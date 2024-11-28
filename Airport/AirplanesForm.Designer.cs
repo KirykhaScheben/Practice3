21e5f8d08fbc29735a09cb3bd382c3f5f66c94ae
KiryukhaScheben <siskabobra2013@gmail.com>
2024-11-28 10:41:11 +0400

    Add files via upload

--- Airport/AirplanesForm.Designer.cs
+++ Airport/AirplanesForm.Designer.cs
@@ -32,6 +32,10 @@
             this.btnLoadFlights = new System.Windows.Forms.Button();
             this.btnLoadRoutes = new System.Windows.Forms.Button();
             this.btnLoadAirplanes = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -74,11 +78,55 @@
             this.btnLoadAirplanes.UseVisualStyleBackColor = true;
             this.btnLoadAirplanes.Click += new System.EventHandler(this.btnLoadAirplanes_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Время полёта";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 395);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Часто летающий самолёт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 453);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 35);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Недозагруженные маршруты";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 453);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Свободные места на рейс 870";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // AirplanesForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLoadAirplanes);
             this.Controls.Add(this.btnLoadFlights);
             this.Controls.Add(this.btnLoadRoutes);
@@ -96,5 +144,9 @@
         private System.Windows.Forms.Button btnLoadFlights;
         private System.Windows.Forms.Button btnLoadRoutes;
         private System.Windows.Forms.Button btnLoadAirplanes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Airport/AirplanesForm.cs
+++ Airport/AirplanesForm.cs
@@ -58,6 +58,156 @@ namespace Airport
             }
         }
 
+        // Запрос 1: Определить среднее расчетное время полета для самолета 'ТУ-154' по маршруту 'Чугуев' - 'Мерефа'
+        private void LoadAverageFlightTime()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=airport.db"))
+            {
+                connection.Open();
+                string selectSql = @"
+        SELECT 
+            Flights.PlaneNumber, 
+            Airplanes.FlightSpeed, 
+            Routes.Distance,
+            AVG(strftime('%s', Flights.ArrivalDateTime) - strftime('%s', Flights.DepartureDateTime)) / 60 AS AverageFlightTime
+        FROM Flights 
+        JOIN Routes ON Flights.RouteNumber = Routes.RouteNumber 
+        JOIN Airplanes ON Flights.PlaneNumber = Airplanes.PlaneNumber
+        WHERE Flights.PlaneNumber = 'TU-154' 
+          AND Routes.DeparturePoint = 'Чугуев' 
+          AND Routes.DestinationPoint = 'Мерефа'
+        GROUP BY Flights.PlaneNumber, Airplanes.FlightSpeed, Routes.Distance";
+
+                using (SQLiteCommand command = new SQLiteCommand(selectSql, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(command.ExecuteReader());
+
+                    // Проверка на наличие данных
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        dataGridView1.DataSource = dataTable;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Нет данных для расчета среднего времени полета для самолета 'ТУ-154' по маршруту 'Чугуев' - 'Мерефа'.");
+                    }
+                }
+            }
+        }
+
+        // Запрос 2: Выбрать марку самолета, которая чаще всего летает по тому же маршруту
+        private void LoadMostFrequentPlane()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=airport.db"))
+            {
+                connection.Open();
+
+                // 1. Найти самолет, который чаще всего летает по маршруту Чугуев - Мерефа
+                string mostFrequentPlaneSql = @"
+            SELECT 
+                Flights.PlaneNumber
+            FROM Flights 
+            JOIN Routes ON Flights.RouteNumber = Routes.RouteNumber
+            WHERE Routes.DeparturePoint = 'Чугуев' AND Routes.DestinationPoint = 'Мерефа'
+            GROUP BY Flights.PlaneNumber
+            ORDER BY COUNT(*) DESC 
+            LIMIT 1";
+
+                string mostFrequentPlane;
+
+                using (SQLiteCommand command = new SQLiteCommand(mostFrequentPlaneSql, connection))
+                {
+                    mostFrequentPlane = command.ExecuteScalar()?.ToString();
+                }
+
+                // 2. Если самолет найден, получить все его рейсы
+                if (!string.IsNullOrEmpty(mostFrequentPlane))
+                {
+                    string flightsSql = @"
+                SELECT 
+                    Flights.PlaneNumber,
+                    Flights.DepartureDateTime,
+                    Flights.ArrivalDateTime,
+                    Routes.DeparturePoint,
+                    Routes.DestinationPoint
+                FROM Flights 
+                JOIN Routes ON Flights.RouteNumber = Routes.RouteNumber
+                WHERE Flights.PlaneNumber = @PlaneNumber";
+
+                    using (SQLiteCommand command = new SQLiteCommand(flightsSql, connection))
+                    {
+                        command.Parameters.AddWithValue("@PlaneNumber", mostFrequentPlane);
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(command.ExecuteReader());
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+        }
+
+        private void LoadUnderbookedRoutes()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=airport.db"))
+            {
+                connection.Open();
+                string selectSql = @"
+        SELECT 
+            Routes.RouteNumber, 
+            Routes.DeparturePoint, 
+            Routes.DestinationPoint,
+            COUNT(Flights.FlightID) AS FlightCount
+        FROM Flights 
+        JOIN Airplanes ON Flights.PlaneNumber = Airplanes.PlaneNumber
+        JOIN Routes ON Flights.RouteNumber = Routes.RouteNumber
+        WHERE Flights.TicketsSold < (Airplanes.Seats * 0.7)
+        GROUP BY Routes.RouteNumber, Routes.DeparturePoint, Routes.DestinationPoint
+        ORDER BY FlightCount DESC
+        LIMIT 1";  // Ограничение на одну запись
+
+                using (SQLiteCommand command = new SQLiteCommand(selectSql, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(command.ExecuteReader());
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+        }
+        // Запрос 4: Определить наличие свободных мест на рейс №870 от 31 декабря 2000 г.
+        private void LoadFreeSeatsForFlight870()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=airport.db"))
+            {
+                connection.Open();
+                string selectSql = @"
+            SELECT 
+                Flights.FlightID,
+                Flights.PlaneNumber,
+                Flights.RouteNumber,
+                Flights.DepartureDateTime,
+                Flights.ArrivalDateTime,
+                Airplanes.Seats,
+                Flights.TicketsSold,
+                (Airplanes.Seats - Flights.TicketsSold) AS FreeSeats 
+            FROM Flights 
+            JOIN Airplanes ON Flights.PlaneNumber = Airplanes.PlaneNumber 
+            WHERE FlightID = 870 AND DepartureDateTime = '2000-12-31 10:00:00'"; // Убедитесь, что время указано правильно
+
+                using (SQLiteCommand command = new SQLiteCommand(selectSql, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(command.ExecuteReader());
+                    dataGridView1.DataSource = dataTable;
+
+                    // Проверка на пустую таблицу
+                    if (dataTable.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Нет доступных мест для рейса №870 на указанную дату.");
+                    }
+                }
+            }
+        }
+
         private void btnLoadAirplanes_Click(object sender, EventArgs e)
         {
             LoadAirplanesData();
@@ -71,6 +221,27 @@ namespace Airport
         private void btnLoadFlights_Click(object sender, EventArgs e)
         {
             LoadFlightsData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadAverageFlightTime();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoadMostFrequentPlane();
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadUnderbookedRoutes();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LoadFreeSeatsForFlight870();
         }
     }
 }
